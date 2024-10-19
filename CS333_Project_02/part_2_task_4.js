2b095c8cdc401ee7c3e9a7493696e6a414f068cc
Jordan Harris Smith <jhsmit25@colby.edu>
2024-10-19 01:02:50 -0400

    Add files via upload

---
+++ CS333_Project_02/part_2_task_4.js
@@ -0,0 +1,17 @@
+/*
+Jordan Smith
+part_2_task_4.js
+09/25/23
+Adds two numbers together and prints out their sum
+*/
+
+function addAndPrint() {
+    const num1 = 5;
+    const num2 = 7;
+    //sums two numbers previously defined and stores it in a new constant variable
+    const sum = num1 + num2;
+    //neatly prints calculations performed in the line above
+    console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+  }
+  
+addAndPrint();
